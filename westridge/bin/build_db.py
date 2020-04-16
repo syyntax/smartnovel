ce78ea194fb682de7117229ef2f49b0749b4b10b
syyntax <syyntax@protonmail.com>
2020-04-15 19:04:39 -0500

    Created function to populate countries table

--- westridge/bin/build_db.py
+++ westridge/bin/build_db.py
@@ -1,6 +1,8 @@
 from pymysql import connect
 from json import loads
 from os.path import abspath, join, dirname
+from sys import exit
+from main import create_address, create_user
 
 
 class connection(object):
@@ -13,15 +15,50 @@ class connection(object):
 
 
 def create_db(conn):
-    # Make the connection to the database
-    cur = conn.cursor()
-    sql_list = loads(open(abspath(join(script_path, '..', 'data', \
-        'tables.json')), 'r').read())
-    for i in sql_list["tables"]:
-        cur.execute(sql_list["tables"][i][0])
-        conn.commit()
+    try:
+        # Make the connection to the database
+        cur = conn.cursor()
+        sql_list = loads(open(abspath(join(script_path, '..', 'data', \
+            'tables.json')), 'r').read())
+        for i in sql_list["tables"]:
+            cur.execute(sql_list["tables"][i][0])
+            conn.commit()
 
-    return None
+        return None
+    except:
+        print(f'An error occurred while creating the database tables.')
+        exit
+    
+'''
+def populate_users(conn, num_users=10):
+    if conn == None:
+        raise Exception(f'conn argument may not be NoneType.')
+    if not type(num_users) is int:
+        raise Exception(f'num_users must be an integer.')
+    if num_users < 1:
+        raise Exception(f'num_users argument must be no less than one.')
+    try:
+        cur = conn.cursor()
+        for i in range(1, num_users):
+            user = create_user()
+            sql = f"INSERT INTO {conn_obj.db} "
+'''
+
+
+def populate_countries(conn):
+    if conn == None:
+        raise Exception(f'conn argument may not be NoneType')
+    try:
+        cur = conn.cursor()
+        countries_list = loads(open(abspath(join(script_path, '..', 'data', \
+            'countries.json')), 'r').read())
+        for i in countries_list:
+            sql = f"INSERT INTO countries (country_name, country_abbrev) " \
+                f"VALUES ('{countries_list[i]}', '{i}');"
+            cur.execute(sql)
+            conn.commit()
+    except:
+        raise Exception(f'An error occurred while populating the countries.')
 
 
 # MAIN
@@ -41,3 +78,4 @@ conn = connect(host=conn_obj.host, user=conn_obj.user, passwd=conn_obj.passwd, \
     db=conn_obj.db, port=conn_obj.port)
 
 create_db(conn)
+populate_countries(conn)
